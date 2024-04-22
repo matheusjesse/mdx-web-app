5d8293485d7faaa3e0e9528bf1c110b26dfe99b7
matheus jessé <matheus_jesse@hotmail.com>
2024-04-22 00:24:12 -0300

    style: cor principal do background da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,8 @@ function App() {
     const { theme } = useContext(ThemeContext);
     const themeName = theme == 'light' ? 'light' : 'dark'
   return (
-    <div className={`${themeName} bg-inverseSurface`}>      
-      <h1 className="text-3xl font-bold underline text-primary">
-      Hello world!
-      </h1>
-      <ToggleThemeButton />
+    <div className={`${themeName} bg-inverseSurface w-full h-screen`}>      
+      
     </ div>
   )
 }
